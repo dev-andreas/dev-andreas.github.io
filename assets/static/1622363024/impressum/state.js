0749bd35b53fb0eb998fef3eaee32831869065c5
Andreas Gerasimow <andreas.gerasimow12@gmail.com>
2021-06-01 20:02:50 +0200

    TTTTTTTTWWWWWWWWEEEEEEEEEAAAAAAAAKKKKKKKKKKSSSSSSS

--- assets/static/1622363024/impressum/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fassets\u002Fstatic\u002F1622363024",layout:"default",error:a,state:{currentLink:-1},serverRendered:true,routePath:"\u002Fimpressum",config:{_app:{basePath:"\u002F",assetsPath:"\u002Fassets\u002F",cdnURL:a}}}}(null));
